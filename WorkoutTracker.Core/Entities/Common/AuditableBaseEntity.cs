d60e3ccf9cf5c04a65848f443f27946ddb0346c9
Rifatur Rahman <35681257+Rifatur@users.noreply.github.com>
2024-08-28 06:48:17 +0600

    Intial Project Workout Tracker

---
+++ WorkoutTracker.Core/Entities/Common/AuditableBaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutTracker.Core.Entities.Common
+{
+    public abstract class AuditableBaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
---
+++ WorkoutTracker.Core/Entities/Exercise.cs
@@ -0,0 +1,12 @@
+﻿using WorkoutTracker.Core.Entities.Common;
+
+namespace WorkoutTracker.Core.Entities
+{
+    public class Exercise : AuditableBaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string MuscleGroup { get; set; } = string.Empty;
+    }
+}
