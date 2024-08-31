7841748113180175bdba314a9b2986e31b8dfa1a
Rifatur Rahman <35681257+Rifatur@users.noreply.github.com>
2024-08-31 23:50:55 +0600

    Workout plan

--- WorkoutTracker.Api/obj/Debug/net8.0/WorkoutTracker.Api.AssemblyInfo.cs
+++ WorkoutTracker.Api/obj/Debug/net8.0/WorkoutTracker.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkoutTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bfc5f0c493ff97e8943fb91c1a54ba7cdde8fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e68c80c82aced3821f8ae3b9f0d47933001288f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkoutTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkoutTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WorkoutTracker.Core/Entities/WorkoutPlan.cs
@@ -0,0 +1,18 @@
+﻿using WorkoutTracker.Core.Entities.Common;
+
+namespace WorkoutTracker.Core.Entities
+{
+    public class WorkoutPlan : AuditableBaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletedDate { get; set; }
+
+
+    }
+}
